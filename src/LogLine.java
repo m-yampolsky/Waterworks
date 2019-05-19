f53b148299ff8608d2b274f3472cd623a26f3f87
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-18 21:46:59 -0400

    Update LogLine.java

--- src/LogLine.java
+++ src/LogLine.java
@@ -1,2 +1,20 @@
-public class LogLine {
+public class LogLine 
+{
+  boolean[][] isColoured;
+  int xCoord, yCoord; //coordinates of the top left corner
+  
+  public LogLine (String pic, int height, int width)
+  {
+    super (pic);
+    PixelReader pixelReader = this.getPixelReader(); 
+    isColoured = new boolean [height][width];
+    for (int y = 0; y < height; y++)
+    {
+      for (int x = 0; x < width; x++)
+      {
+        if (!pixelReader.getColor(x, y).equals(Color.TRANSPARENT))
+          isColoured[y][x] = true;
+      }
+    }
+  }
 }
