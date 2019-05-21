dd3b9829a1906d8a81c620b1142b755548fc0495
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 20:55:06 -0400

    Javadoc

--- src/LogLine.java
+++ src/LogLine.java
@@ -3,19 +3,22 @@ import javafx.scene.paint.Color;
 import javafx.scene.image.PixelReader;
 
 /**
- *
+ * The LogLine class
+ * This class stores an object that represents the logs that the avatar will have to run across during the game
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 public class LogLine extends Image
 {
   /**
-   *
+   * This array stores whether or not each pixel of the image has a colour at a spot or not
    */
   private boolean[][] isColoured;
 
   /**
-   *
+   * The coordinates of the top left corner
    */
-  private int xCoord, yCoord; //coordinates of the top left corner
+  private int xCoord, yCoord; 
 
   /**
    * @param pic
@@ -38,7 +41,7 @@ public class LogLine extends Image
   /**
    * @param x
    * @param y
-   * @return
+   * @return true if the pixel at that coordinate is coloured, or false if it is transparent
    */
   public boolean isColoured (int x, int y) {
     return isColoured[x][y];
