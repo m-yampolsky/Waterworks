45b295bcc5ab96e77699e7c839009d309a5c7330
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 20:50:23 -0400

    Update Learn.java

--- src/Learn.java
+++ src/Learn.java
@@ -1,5 +1,8 @@
 /**
- *
+ * The Learn class
+ * This class will store an object that represents the window where the user will be learning about dealing with water overusage.
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 public class Learn {
 }
