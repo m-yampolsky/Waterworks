c86ff882ff476232ab01cdebb1f2c815a35058dd
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 20:44:43 -0400

    Javadoc

--- src/DeviceLine.java
+++ src/DeviceLine.java
@@ -1,5 +1,8 @@
 /**
- *
+ * The DeviceLine class
+ * This class stores the image of the list of efficient and inefficient devices appearing on screen during the game.
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 public class DeviceLine {
 }
