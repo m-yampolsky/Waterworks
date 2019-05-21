a44cbe91b017847c60e8a574ce8424e0c36374e7
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 20:43:33 -0400

    Update Cloud.java

--- src/Cloud.java
+++ src/Cloud.java
@@ -1,5 +1,8 @@
 /**
- *
+ * The Cloud class
+ * This class stores a Cloud object to represent the image of a cloud moving around on screen during the game.
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 public class Cloud {
 }
