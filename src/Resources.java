89475d974fdf644dc56cf7f96b375cc9a0fdbfb3
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 21:06:42 -0400

    Javadoc

--- src/Resources.java
+++ src/Resources.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 
 /**
- *
+ * The Resources class
+ * This class stores a HashMap that stores all the resources used during the program, to save loading time during the game.
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 public class Resources {
     /**
