ae8ac52a2b3ca8d736bf926247f7a443b52beb2d
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 20:57:50 -0400

    Javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import javafx.application.Application;
 
 /**
- *
+ * The Main class
+ * This is the driver class of the Waterworks program.
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 class Main {
+    /**
+    * The main method of the class
+    * @param args The program
+    */
     public static void main(String[] args) {
         Application.launch(StageManager.class, args);
     }
