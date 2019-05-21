cb49fc0631016ced3f93277cf7396e69506ae41d
Maria Yampolsky <50776467+27csj@users.noreply.github.com>
2019-05-20 21:03:33 -0400

    Javadoc

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,5 +1,8 @@
 /**
- *
+ * The Quiz class
+ * This class represents the Quiz window of the game where the user is tested on what they learned about water conservation
+ * @author Maria Yampolsky and Vansh Juneja
+ * @version 1 05.20.2019
  */
 public class Quiz {
 }
