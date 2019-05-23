9a64fa59b6a6a27a465e4eb2e3249e02bab673f6
maria <maria.yampolsky@gmail.com>
2019-05-23 18:22:00 -0400

    Quiz Dragging

--- src/Quiz.java
+++ src/Quiz.java
@@ -30,7 +30,7 @@ public class Quiz extends Window {
 
     public void display() {
 
-        Image background = (Image) (Resources.get("quizBack"));
+        ImageView background = (ImageView) (Resources.get("quizBack"));
         ImageView menuBtn = (ImageView)(Resources.get("menuBtn"));
 
         ImageView tub = (ImageView)(Resources.get("tub"));
@@ -69,153 +69,62 @@ public class Quiz extends Window {
         drawImage (eBox, -350, 200);
         drawImage (iBox, 360, 200);
 
-        
-        //mouse pressed actions
-        /*dSponge.setOnMouseClicked(e -> {
-            device = dSponge;
-            e.setDragDetect (true);
-            isDragged = true;
-        });
 
-        dSponge.setOnDragDetected(new EventHandler <MouseEvent>() {
-            public void handle(MouseEvent event) {
-                // drag was detected, start drag-and-drop gesture
-                System.out.println("onDragDetected");
-                device = dSponge;
-                // allow any transfer mode
-                Dragboard db = dSponge.startDragAndDrop(TransferMode.ANY);
+        ImageView[] devices = {dSponge, sink, shower, wCan, barrel, hose, cWasher, tub, dWasher};
 
-                // put a string on dragboard
-                ClipboardContent content = new ClipboardContent();
-                content.putImage(dSpongeImg);
-                db.setContent(content);
-
-                event.consume();
-            }
-        });
-
-
-        eBox.setOnDragOver(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                // data is dragged over the target
-                System.out.println("onDragOver");
-
-                // accept it only if it is  not dragged from the same node and if it has a string data
-                if (event.getGestureSource() != eBox &&
-                        event.getDragboard().hasImage()) {
-                    // allow for both copying and moving, whatever user chooses
-                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                }
-                remove (device);
-                drawImage (device, -350, 200);
-
-                event.consume();
-            }
-        });
-
-
-        eBox.setOnDragEntered(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                // the drag-and-drop gesture entered the target
-                System.out.println("onDragEntered");
-                // show to the user that it is an actual gesture target
-                if (event.getGestureSource() != eBox &&
-                        event.getDragboard().hasImage()) {
-                    remove (device);
-                }
-
-                event.consume();
-            }
-        });
-
-        eBox.setOnDragExited(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                // mouse moved away, remove the graphical cues
-                drawImage (dSponge, -110, -185 );
-
-                event.consume();
-            }
-        });
-
-        eBox.setOnDragDropped(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                // data dropped
-                System.out.println("onDragDropped");
-                // if there is a string data on dragboard, read it and use it
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasImage()) {
-                    remove (device);
-                    success = true;
-                    drawImage (device, -350, 200);
-                }
-                // let the source know whether the string was successfully transferred and used
-                event.setDropCompleted(success);
-
-                event.consume();
-            }
-        });
-
-        dSponge.setOnDragDone(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                // the drag-and-drop gesture ended
-                System.out.println("onDragDone");
-                // if the data was successfully moved, clear it
-                if (event.getTransferMode() == TransferMode.MOVE) {
-                    remove (dSponge);
-                }
-
-                event.consume();
-            }
-        });*/
-
-
-        dSponge.setOnMousePressed(new EventHandler <MouseEvent>()
+        for (ImageView img : devices)
         {
-            public void handle(MouseEvent event)
-            {
-                dSponge.setMouseTransparent(true);
-                System.out.println ("Event on Source: mouse pressed");
-                event.setDragDetect(true);
-            }
-        });
+            img.setOnMousePressed(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    img.setMouseTransparent(true);
+                    isDragged = false;
+                    device = img;
+                    event.setDragDetect(true);
+                }
+            });
 
-        dSponge.setOnMouseReleased(new EventHandler <MouseEvent>()
-        {
-            public void handle(MouseEvent event)
-            {
-                dSponge.setMouseTransparent(false);
-                System.out.println("Event on Source: mouse released");
-            }
-        });
+            img.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    img.setMouseTransparent(false);
+                    isDragged = false;
+                    device = null;
+                }
+            });
 
-        dSponge.setOnMouseDragged(new EventHandler <MouseEvent>()
-        {
-            public void handle(MouseEvent event)
-            {
-                System.out.println("Event on Source: mouse dragged");
-                event.setDragDetect(false);
-            }
-        });
+            img.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    event.setDragDetect(false);
+                    x = (int) event.getSceneX();
+                    y = (int) event.getSceneY();
+                    isDragged = true;
+                }
+            });
 
-        dSponge.setOnDragDetected(new EventHandler <MouseEvent>()
-        {
-            public void handle(MouseEvent event)
-            {
-                dSponge.startFullDrag();
-                System.out.println("Event on Source: drag detected");
-            }
-        });
-
+            img.setOnDragDetected(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    img.startFullDrag();
+                }
+            });
+        }
 
 
         eBox.setOnMouseDragEntered(new EventHandler <MouseDragEvent>()
         {
             public void handle(MouseDragEvent event)
             {
-                remove (dSponge);
-                drawImage (dSponge, (int)event.getSceneX(), (int)event.getSceneY());
-                System.out.println("Event on Target: mouse dragged");
+                isDragged = true;
+                x = (int)event.getSceneX();
+                y = (int)event.getSceneY();
+            }
+        });
+
+        background.setOnMouseDragOver(new EventHandler <MouseDragEvent>()
+        {
+            public void handle(MouseDragEvent event)
+            {
+                x = (int)event.getSceneX();
+                y = (int)event.getSceneY();
+                isDragged = true;
             }
         });
 
@@ -223,9 +132,9 @@ public class Quiz extends Window {
         {
             public void handle(MouseDragEvent event)
             {
-                remove (dSponge);
-                drawImage (dSponge, (int)event.getSceneX(), (int)event.getSceneY());
-               System.out.println("Event on Target: mouse drag over");
+                x = (int)event.getSceneX();
+                y = (int)event.getSceneY();
+                isDragged = true;
             }
         });
 
@@ -233,59 +142,43 @@ public class Quiz extends Window {
         {
             public void handle(MouseDragEvent event)
             {
-                remove (dSponge);
-                drawImage (dSponge, -350, 200);
+                remove (device);
+                drawImage (device, -350, 150);
                 remove (eBox);
                 drawImage (eBox, -350, 200);
-                System.out.println("Event on Target: mouse drag released");
+                device = null;
             }
         });
 
+        background.setOnMouseDragReleased(new EventHandler <MouseDragEvent>()
+        {
+            public void handle(MouseDragEvent event)
+            {
+                remove (device);
+                drawImage (device,-110, -185  );
+            }
+        });
+        
         eBox.setOnMouseDragExited(new EventHandler <MouseDragEvent>()
         {
             public void handle(MouseDragEvent event)
             {
-                System.out.println("Event on Target: mouse drag exited");
             }
         });
 
 
 
 
-       /* final long startNanoTime = System.nanoTime();
+       final long startNanoTime = System.nanoTime();
         new AnimationTimer() {
             public void handle(long currentNanoTime) {
                 double t = (currentNanoTime - startNanoTime) / 300000000.0;
 
-               if (device != null && isDragged)
-                {
-                    remove (device);
-                    drawImage (device, x, y);
+                if (isDragged && device != null) {
+                    remove(device);
+                    drawImage(device, x - 500, y - 375);
                 }
-
-                if (!reachedBox && device != null && !isDragged)
-                {
-                    if (device.equals(dSponge))
-                        drawImage (dSponge, -110, -185 );
-                    else if (device.equals(sink))
-                        drawImage (sink,17, -179 );
-                    else if (device.equals(shower))
-                        drawImage (shower, 128, -188);
-                    else if (device.equals(wCan))
-                        drawImage (wCan, -90, -52);
-                    else if (device.equals(barrel))
-                        drawImage (barrel, 37, -52);
-                    else if (device.equals(hose))
-                        drawImage (hose, 138, -48);
-                    else if (device.equals(cWasher))
-                        drawImage (cWasher, -130, 75);
-                    else if (device.equals(tub))
-                        drawImage (tub, 5, 82);
-                    else if (device.equals(dWasher))
-                        drawImage (dWasher, 145, 76);
-                }
-
             }
-        }.start();*/
+        }.start();
     }
 }
