8a5e1e73aef7d5acbcf2536bc05b086837f1292f
Vansh Juneja <vanshjuneja03@gmail.com>
2019-06-06 21:47:10 -0400

    Resources and Main javadoc

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
  * The Main class
  * This is the driver class of the Waterworks program.
  * @author Maria Yampolsky and Vansh Juneja
- * @version 2 05.27.2019
+ * @version 5 06.06.2019
+ *
+ * Version History
+ * May 18:
+ * Maria created Main class with public static void main implementation.
  */
 class Main {
     /**
-    * The main method of the class
+    * The main method of the class which launches the StageManager Application class.
     * @param args The program
     */
     public static void main(String[] args) {
